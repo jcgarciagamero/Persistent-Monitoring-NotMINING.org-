db5bfc226c828efaaff79334c53bde5344a86c65
José C. García Gamero <jcgarciagamero@gmail.com>
2018-06-05 18:21:15 +0200

    Update persistentMonitoring.py

--- persistentMonitoring.py
+++ persistentMonitoring.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
-#Version: 0.0.7
+#Version: 2.0.0
 
 import urllib
 import psutil
 import os
 import time
 import sys
+from PyQt4 import QtGui  
+from PyQt4.QtGui import QMessageBox  
+aplicacion = QtGui.QApplication(sys.argv)
+
 
 i = 0
 while i == 0:
-	cpu = psutil.cpu_percent()
+	cpu = psutil.cpu_percent()	
 	time.sleep( 1 )
 	if cpu > 30.0:
 			
@@ -17,126 +21,18 @@ while i == 0:
 			os.system('netstat > net.html')
 			scan = "net.html"
 			lines = urllib.urlopen(scan).read()
-			search = lines.find('coinhive')
-			search1 = lines.find('rs-solution.de')
-			search2 = lines.find('ns3033653.ip-164')
-			search3 = lines.find('104.25.6.31')
-			search4 = lines.find('104.24.104.76')
-			search5 = lines.find('104.18.55.211')
-			search6 = lines.find('104.27.185.32')
-			search7 = lines.find('ns546545.ip-158-6')
-			search8 = lines.find('beta00.cortacoin')
-			search9 = lines.find('mail.aba.ae')
-			search10 = lines.find('104.31.93.36')
-			search11 = lines.find('104.31.92.36')
-			search12 = lines.find('ec2-52-57-80-78')
-			search13 = lines.find('5.255.86.116')
-			search14 = lines.find('45.77.196.10')
-			search15 = lines.find('45.63.109.36')
-			search16 = lines.find('207.246.116.117')
-			search17 = lines.find('45.77.192.104')
-			search18 = lines.find('188.166.33.242')			
-			search19 = lines.find('178.62.227.52')	 		
-			search20 = lines.find('104.18.46.158')
-			search21 = lines.find('104.27.152.155')
-			search22 = lines.find('104.18.54.211')
-			search23 = lines.find('crypto-webminer')
-			search24 = lines.find('104.28.16.102')
-			search25 = lines.find('ns3083487.ip-145')
-			search26 = lines.find('ns3104461.ip-54')
-			search27 = lines.find('185.80.53.183')
-			search28 = lines.find('188.42.240.146')
-			search29 = lines.find('165.227.10.77')
-			search30 = lines.find('207.246.117.237')
-			search31 = lines.find('206.189.24.193')
-			if search != -1:
-				print('Mining with Coinhive')
+			ip = (['coinhive','rs-solution.de','ns3033653.ip-164','104.25.6.31','104.24.104.76','104.18.55.211','104.27.185.32','ns546545.ip-158-6','beta00.cortacoin','mail.aba.ae',
+			'104.31.93.36','104.31.92.36','ec2-52-57-80-78','5.255.86.116','45.77.196.10','45.63.109.36','207.246.116.117','45.77.192.104','188.166.33.242','178.62.227.52','104.18.46.158',
+			'104.27.152.155','104.18.54.211','crypto-webminer','104.28.16.102','ns3083487.ip-145','ns3104461.ip-54','185.80.53.183','188.42.240.146','165.227.10.77','207.246.117.237',
+			'206.189.24.193','104.28.15.29'])
+			search = lines.find(str(ip))
+			for search in ip:
 				
-			elif search1 != -1:
-				print('Mining with gustaver.ddns.net')
-				
-			elif search2 != -1:
-				print('Mining with pon.ewtuyytdf45.com')
-				
-			elif search3 != -1:
-				print('Mining with crypto-loot.com')
-				
-			elif search4 != -1:
-				print('Mining with mepirtedic.com')
-				
-			elif search5 != -1:
-				print('Mining with aster18cdn.nl')
-				
-			elif search6 != -1:
-				print('Mining with cdn.whysoserius.club')
-				
-			elif search7 != -1:
-				print('Mining with gtg02.bestsecurepractice.com')
-				
-			elif search8 != -1:
-				print('Mining with beta00.cortacoin.com')
-				
-			elif search9 != -1:
-				print('Mining with "mail.aba.ae" used by: freecontent.*, hashing.win, webassembly.stream, coinimp.com...')
-				
-			elif search10 != -1:
-				print('Mining with crypto-loot.com')
-				
-			elif search11 != -1:
-				print('Mining with crypta-loot.com')
-			
-			elif search12 != -1:
-				print('Mining with webminerpool.com')
-			
-			elif search13 != -1:
-				print('Mining with Proxy: wss://javascriptcdn.stream:8892/proxy , Coinhive')
-			
-			elif search14 != -1:
-				print('Mining with herphemiste.com')
-			
-			elif search15 != -1:
-				print('Mining with herphemiste.com')
-			
-			elif search16 != -1:
-				print('Mining with herphemiste.com')
-			
-			elif search17 != -1:
-				print('Mining with herphemiste.com')
-			
-			elif search18 != -1:
-				print('Mining with web.stati.bid')
-			
-			elif search19 != -1:
-				print('Mining with g-content.bid')
-			
-			elif search20 != -1:
-				print('Mining with coin-have.com')
-			
-			elif search21 != -1:
-				print('Mining with 1q2w3.website')
-			
-			elif search22 != -1:
-				print('Mining with aster18cdn.nl')
-			
-			elif search23 != -1:
-				print('Mining with eth-pocket.de')
-			
-			elif search24 != -1:
-				print('Mining with tulip18.com/amo.js')
-			
-			elif search25 != -1:
-				print('Mining with mineralt.io')
-			elif search26 != -1:
-				print('Mining with mineralt.io')
-			elif search27 != -1:
-				print('Mining with coincube.io')
-			elif search28 != -1:
-				print('Mining with coincube.io')
-			elif search29 != -1:
-				print('Mining with munero.me')
-			elif search30 != -1:
-				print('Mining with mb.sentemanactri.com')
-			elif search31 != -1:
-				print('Mining with polskota.club')
-									
+				if search != -1:
+					print('Miner Detected')
+					os.system('echo Miner Detected  >> logs.txt')
+					os.system('date >> logs.txt')
+					break
+			os.system('rm -rf net.html')			
 			time.sleep( 10 )
+			
